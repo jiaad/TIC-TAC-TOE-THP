dc1bc4b3fe76d8fb4c912e4debfde1567e973b38
jiaad <abdulziad08@outlook.com>
2019-01-24 10:48:05 +0100

    ajoute

--- lib/board_case.rb
+++ lib/board_case.rb
@@ -2,8 +2,10 @@ class BoardCase
   #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
 attr_accessor :value , :identifiant
   
-  def initialize
+  def initialize(value=("X" || "0" || "" , identifiant() ))
     #TO DO : doit régler sa valeur, ainsi que son numéro de case
+    @value = value
+    @identifiant = identifiant
   end
   
 end
